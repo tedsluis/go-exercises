0facac370df5e094420cca2f6bbba62926db068e
tedsluis <ted.sluis@gmail.com>
2016-11-13 16:00:59 +0100

    change code with: io.Copy(os.Stdout, resp.Body)

--- fetch-1.7.go
+++ fetch-1.7.go
@@ -3,7 +3,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
@@ -12,16 +13,19 @@ func main() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			fmt.Fprintf(os.Stderr, "fetch error, Get(url): %v\n", err)
 			os.Exit(1)
 		}
 		//b, err := ioutil.ReadAll(resp.Body)
-		b, err := io.Copy(resp.Body,
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch error: reading %s: %v\n", url, err)
 			os.Exit(1)
-		}
-		fmt.Printf("%s", b)
+   		}
+		resp.Body.Close()
+		//if err != nil {
+		//	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		//	os.Exit(1)
+		//}
+		//fmt.Printf("%s", b)
 	}
 }
