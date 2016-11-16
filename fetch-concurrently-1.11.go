4f1ffb0931492f7c3c430eda7715f81cb10dbf1c
Ted Sluis <ted.sluis@gmail.com>
2016-11-16 19:28:43 +0100

    added errors to output. formatted output

--- fetch-concurrently-1.11.go
+++ fetch-concurrently-1.11.go
@@ -16,11 +16,13 @@ func main() {
 	start := time.Now()
 	ch := make(chan string)
 	startcount := 1
+	prefix := 1
 	for _, url := range os.Args[1:] {
 		for times := 0; times < 10; times++ {
-			go fetch(url, ch, startcount, times) // start a goroutine
-			startcount++;
+			go fetch(url, ch, startcount, times, prefix) // start a goroutine
+			startcount++
 		}
+		prefix++
 	}
 	var sortindex [] string
 	lines := make(map[string]string)
@@ -28,28 +30,29 @@ func main() {
 		result := strings.Split(<-ch,";") // receive from channel ch
 		index := fmt.Sprintf("%-20s %4s %4s", result[3], result[1], result[0])
 		sortindex = append(sortindex,index)
-		lines[index]=fmt.Sprintf("%-20s order=%4s overall=%4s sec=%4s nbytes=%7s", result[3],result[4],result[0],result[1],result[2])
+		lines[index]=fmt.Sprintf("%-25s order=%7s overall=%4s sec=%4s nbytes=%7s, message=%s", result[3],result[4],result[0],result[1],result[2],result[5])
 	}
 	sort.Strings(sortindex)
 	for _,index := range sortindex {
-		fmt.Fprintf(os.Stderr,"-> %-30s -> %-60s \n", index,lines[index])
-		//fmt.Println(">"+index+">"+lines[index])
+		fmt.Fprintf(os.Stderr,"%-60s \n", lines[index])
 	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
-func fetch(url string, ch chan<- string, count int, times int) {
+func fetch(url string, ch chan<- string, count int, times int, prefix int) {
 	start := time.Now()
 	resp, err := http.Get(url)
+	secs := time.Since(start).Seconds()
 	if err != nil {
-		ch <- fmt.Sprintf("%4d;%.2f;%30s;%s;%4d", count, secs, err, url, times) // send to channel ch
+		ch <- fmt.Sprintf("%4d;%.2f;%7d;%s;%2d/%-4d;%s", count, secs, 0, url, prefix, times, err) // send to channel ch
 		return
 	}
 	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close() // don't leak resources
+	secs = time.Since(start).Seconds()
 	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		ch <- fmt.Sprintf("%4d;%.2f;%7d;%s;%2d/%-4d;%s", count, secs, 0, url, prefix, times, err) // send to channel ch
 		return
 	}
-	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%4d;%.2f;%7d;%s;%4d", count, secs, nbytes, url, times)
+	secs = time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%4d;%.2f;%7d;%s;%2d/%-4d;%s", count, secs, nbytes, url, prefix, times, "ok")
 }
