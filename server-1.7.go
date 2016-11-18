afb92956e7509f76c4cb81d55bbe6d7fbdbb2984
Ted Sluis <ted.sluis@gmail.com>
2016-11-18 07:37:01 +0100

    added http info

--- server-1.7.go
+++ server-1.7.go
@@ -12,22 +12,12 @@ import (
 var mu sync.Mutex
 var count int
 var start = time.Now()
-var last  = time.Now()
+var last = time.Now()
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-}
-
-// handler echoes the Path component of the requested URL.
-func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	last = start
-	start = time.Now()
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	log.Fatal(http.ListenAndServe("192.168.1.42:8000", nil))
 }
 
 // counter echoes the number of calls so far.
@@ -35,4 +25,26 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "Count=%d\nt1=%v\nt2=%v", count, time.Since(last).Seconds(), time.Since(start).Seconds())
 	mu.Unlock()
+}
+// handler echoes the HTTP request.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+	mu.Lock()
+	count++
+	last = start
+	start = time.Now()
+	fmt.Fprintf(w, "Count=%d\nt1=%v\nt2=%v", count, time.Since(last).Seconds(), time.Since(start).Seconds())
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
