a8b26b8a8509fc532b533e526e9ea0eab1f35944
tedsluis <ted.sluis@gmail.com>
2016-11-20 18:15:00 +0100

    added lower and uppercase

--- echo-2.3.2.go
+++ echo-2.3.2.go
@@ -1,16 +1,28 @@
 // Echo4 prints its command-line arguments.
 package main
+
 import (
-    "flag"
-    "fmt"
-    "strings"
+	"flag"
+	"fmt"
+	"strings"
 )
+
 var n = flag.Bool("n", false, "omit trailing newline")
 var sep = flag.String("s", " ", "separator")
+var u = flag.Bool("u", false, "convert to uppercase")
+var l = flag.Bool("l", false, "convert to lowercase")
+
 func main() {
-    flag.Parse()
-    fmt.Print(strings.Join(flag.Args(), *sep))
-    if !*n {
-        fmt.Println()
-    }
+	flag.Parse()
+	text := strings.Join(flag.Args(), *sep)
+	if *u == true {
+		text = strings.ToUpper(text)
+	}
+	if *l == true {
+		text = strings.ToLower(text)
+	}
+	fmt.Print(text)
+	if !*n {
+		fmt.Println()
+	}
 }
