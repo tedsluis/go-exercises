dd0f27342f83ecfbe12c2c7424454be278be79e2
Ted Sluis <ted.sluis@gmail.com>
2016-11-21 21:41:03 +0100

    methode ct.string

--- boiling-3.go
+++ boiling-3.go
@@ -29,11 +29,22 @@ func main() {
 
 	var c Celsius
 	var f Fahrenheit
-	fmt.Println("c == 0", c == 0)             // "true"
-	fmt.Println("f >= 0", f >= 0)             // "true"
+	fmt.Println("c == 0", c == 0)                         // "true"
+	fmt.Println("f >= 0", f >= 0)                         // "true"
 	fmt.Println("Fahrenheit(c) == f", Fahrenheit(c) == f) // "true"
-	fmt.Println("c == Celsius(f)"   , c == Celsius(f))    // "true"!
+	fmt.Println("c == Celsius(f)", c == Celsius(f))       // "true"!
+
+	ct := FToC(212.0)
+	fmt.Println("ct.String()=",ct.String()) // "100°C"
+	fmt.Printf("v ct=%v\n", ct)             // "100°C"; no need to call String explicitly
+	fmt.Printf("s ct=%s\n", ct)             // "100°C"
+	fmt.Println("ct=",ct)                   // "100°C"
+	fmt.Printf("g ct=%g\n", ct)             // "100"; does not call String
+	fmt.Println("float64(ct)=",float64(ct)) // "100"; does not call String
+
 }
+
+func (ct Celsius) String() string { return fmt.Sprintf("%g°C", ct) }
 
 func fToC(f float64) float64 {
 	return (f - 32) * 5 / 9
