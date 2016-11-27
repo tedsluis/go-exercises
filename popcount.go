a5f1d47a4530652de95b138c6d6e1a07c65f3e07
Ted Sluis <ted.sluis@gmail.com>
2016-11-27 15:28:27 +0100

    count bits

---
+++ popcount.go
@@ -0,0 +1,22 @@
+// Popcount, bit count
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"popcount"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		x, err := strconv.ParseUint(arg, 10, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		y := popcount.PopCount(x)
+		fmt.Println("x=", x, ", y=", y)
+	}
+}
