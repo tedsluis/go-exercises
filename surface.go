f7f137ba4af43b99e88512434d2dba2bf1e7cf04
Ted Sluis <ted.sluis@gmail.com>
2016-12-05 21:33:33 +0100

    changed parameters

--- surface.go
+++ surface.go
@@ -8,8 +8,8 @@ import (
 
 const (
 	width, height = 600, 320            // canvas size in pixels
-	cells         = 100                 // number of grid cells
-	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	cells         = 200                 // number of grid cells
+	xyrange       = 90.0                // axis ranges (-xyrange..+xyrange)
 	xyscale       = width / 2 / xyrange // pixels per x or y unit
 	zscale        = height * 0.4        // pixels per z unit
 	angle         = math.Pi / 6         // angle of x, y axes (=30°)
