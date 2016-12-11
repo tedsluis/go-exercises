3be6ec9916cbe6cd9fad75ce5cab9a9470ac1fec
tedsluis <ted.sluis@gmail.com>
2016-12-11 18:23:20 +0100

    mandelbrot in color

---
+++ mandelbrot2.go
@@ -0,0 +1,41 @@
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 4048, 4048
+	)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.RGBA {
+	const iterations = 200
+	const contrast = 15
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.RGBA{110, 200, 20, contrast * n}
+		}
+	}
+	return color.RGBA{255, 255, 255, 255}
+}
